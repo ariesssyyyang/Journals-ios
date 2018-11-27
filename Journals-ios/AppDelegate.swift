ce6113e99961bdceb95326f72da52450058dc023
ariesssyyyang <ariesssyyyang@gmail.com>
2018-11-28 01:21:34 +0800

    Initialize application window

--- Journals-ios/AppDelegate.swift
+++ Journals-ios/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = JournalListController()
+        window.makeKeyAndVisible()
+        self.window = window
+
         return true
     }
 
---
+++ Journals-ios/JournalListController.swift
@@ -0,0 +1,16 @@
+//
+//  JournalListController.swift
+//  Journals-ios
+//
+//  Created by Aries Yang on 2018/11/28.
+//  Copyright © 2018 Aries Yang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JournalListController: UIViewController {
+    override func viewDidLoad() {
+        self.view.backgroundColor = .white
+    }
+}
