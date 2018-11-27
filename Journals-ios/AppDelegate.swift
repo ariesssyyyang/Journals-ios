0fd2eaacaf0f3b011cedb6bffcf6b2b3f732f98a
ariesssyyyang <ariesssyyyang@gmail.com>
2018-11-28 01:11:16 +0800

    Clean project

--- Journals-ios/AppDelegate.swift
+++ Journals-ios/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,15 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-
-        Database.database().isPersistenceEnabled = true
-
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-
-        window?.rootViewController = JournalsListController()
-
         return true
     }
 
